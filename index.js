64c8fa46358f63d226a905939178d84de5cd3a2f
Travis Lavery <travisalavery@gmail.com>
2017-11-21 01:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const app = "I don't do much."
 var kittens = []
 
-function destructivelyAppendKitten(name, array) {
-  array.push(name)
-  return array
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
 }
 
 function destructivelyPrependKitten(name, array) {
